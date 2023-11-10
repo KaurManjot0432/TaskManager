153dbca3afd2a2a38ffa4d971762298934a796cd
KaurManjot0432 <0432manjot@gmail.com>
2023-11-10 17:54:29 +0530

    added create task, delete task, edit task and getting feed task functionality at client side

--- src/scenes/CreateArea/CreateArea.tsx
+++ src/scenes/CreateArea/CreateArea.tsx
@@ -1,11 +1,142 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './CreateArea.css';
+import AddIcon from '@mui/icons-material/Add';
+import { useSelector } from "react-redux";
+import { IconButton, MenuItem, Select, Card, CardContent } from "@mui/material";
+import TaskItem from "./TaskItem";
+import { useDispatch } from "react-redux";
+import { setTasks } from "../../state";
+import config from "../../config";
 
-const CreateArea = () => {
+function CreateArea() {
+    interface Token {
+        token: string
+    }
+
+    interface FeedTask {
+        tasks: Array<
+            {
+                id: string,
+                title: string,
+                description: string,
+            }>
+    }
+
+    const [postUpdated, setPostupdated] = useState(false);
+    const [isExpand, setExpand] = useState(false);
+    const dispatch = useDispatch()
+    const token = useSelector((state: Token) => state?.token);
+    const feedTasks = useSelector((state: FeedTask) => state.tasks);
+
+
+    const [text, setText] = useState({
+        title: "",
+        description: "",
+        completed: false
+    });
+
+    function expanded() {
+        setExpand(true)
+    }
+    const handlePostClick = async (e: any) => {
+        e.preventDefault()
+        try {
+            const addPost = await fetch(`${config.apiUrl}/task`, {
+                method: "POST",
+                headers: {
+                    'auth-token': token,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(text),
+            });
+            const addedPost = await addPost.json();
+            if (addedPost.success) {
+                setText({
+                    title: '',
+                    description: '',
+                    completed: false
+                });
+                setPostupdated(prevState => !prevState);
+            } else {
+                console.error('Failed to add post');
+            }
+        } catch (error) {
+            console.error('Error adding post:', error);
+
+        }
+    };
+
+    const getMyPosts = async () => {
+        try {
+            const postResponse = await fetch(`${config.apiUrl}/task`, {
+                method: "GET",
+                headers: { 'auth-token': token }
+            });
+            const data = await postResponse.json()
+            dispatch(setTasks({ tasks: data.response }))
+        } catch (err) {
+            console.log("get feedTasks error", err)
+        }
+    }
+
+    const handleTextChange = (event: any) => {
+        const { name, value } = event.target;
+        setText((prevText) => ({ ...prevText, [name]: value }));
+    };
+
+    useEffect(() => {
+        getMyPosts();
+    }, [postUpdated]);
+
     return (
-        <>
-        This is create area
-        </>
-    )
+        <div data-testid='createarea' className="createAreaContainer">
+            <form className="createAreaForm" onSubmit={handlePostClick}>
+                <input
+                    className="createAreaInput"
+                    onClick={expanded}
+                    onChange={event => {
+                        const { name, value } = event.target;
+                        setText(() => { return { ...text, [name]: value } })
+                    }}
+                    name="title"
+                    placeholder="To do title"
+                    value={text.title}
+                />
+                {isExpand && (
+                    <div className="expandedFields">
+                        <input
+                            onChange={handleTextChange}
+                            name="description"
+                            placeholder="Add a short description..."
+                            value={text.description}
+                            className="descriptionInput"
+                        />
+                    </div>
+                )}
+                <IconButton type="submit" className="submitButton">
+                    <AddIcon className="AddIcon" />
+                </IconButton>
+            </form>
+            {Array.isArray(feedTasks) ? (
+                <div className="taskItemList">
+                    {feedTasks.map((post: any) => (
+                        <Card key={post.id} className="taskCard">
+                            <CardContent>
+                                <TaskItem
+                                    completed={post.completed}
+                                    postId={post.id}
+                                    title={post.title}
+                                    content={post.description}
+                                />
+                            </CardContent>
+                        </Card>
+                    ))}
+                </div>
+            ) : (
+                <div></div>
+            )}
+        </div>
+    );
 }
 
 export default CreateArea;
---
+++ src/scenes/CreateArea/TaskItem.tsx
@@ -0,0 +1,209 @@
+import React from 'react';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Typography from '@mui/material/Typography';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setTasks, setTask, setDelete } from "../../state";
+import CheckIcon from '@mui/icons-material/Check';
+import config from '../../config';
+
+interface TaskItemProperty {
+    completed: boolean,
+    postId: string,
+    title: string,
+    content: string
+}
+interface State {
+    _id: string
+}
+interface Token {
+    token: string
+}
+function TaskItem({ completed, postId, title, content }: TaskItemProperty) {
+    const token = useSelector((state: Token) => state.token)
+    const dispatch = useDispatch();
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [deleteClicked, setDeleteClicked] = useState(false)
+    const [isChecked, setChecked] = useState(true);
+    const [heading, setTitle] = useState(title);
+    const [descreption, setContent] = useState(content);
+    const taskItemStyles: React.CSSProperties = {
+        border: '1px solid #ccc',
+        padding: '10px',
+        marginBottom: '10px',
+        borderRadius: '4px',
+        backgroundColor: '#f9f9f9',
+        width: '518px',
+        marginLeft: '378px',
+        position: 'relative',
+    };
+    const taskcheckedItemStyles: React.CSSProperties = {
+        border: '3px solid black ',
+        padding: '10px',
+        marginBottom: '10px',
+        borderRadius: '4px',
+        backgroundColor: '#f9f9f9',
+        width: '518px',
+        marginLeft: '378px',
+        position: 'relative',
+    };
+
+    const iconsContainerStyles: React.CSSProperties = {
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        display: 'flex',
+        alignItems: 'center',
+    };
+
+    const titleStyles: React.CSSProperties = {
+        fontSize: '1.2rem',
+        fontWeight: 'bold',
+    };
+
+
+    const checkStatus = async (e: any) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`${config.apiUrl}/post/${postId}/checked`, {
+                method: "PATCH",
+                headers: {
+                    "Authorization": `Bearer ${token}`,
+                },
+
+            });
+
+            if (!response.ok) {
+                throw new Error('Request failed');
+            }
+
+            const updatedPost = await response.json();
+            dispatch(setTask({ post: updatedPost }));
+
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    }
+    const handleDelete = async (e: any) => {
+        e.preventDefault();
+        setDeleteClicked(!deleteClicked);
+        try {
+            console.log(postId);
+            const response = await fetch(`${config.apiUrl}/task/deleteTask/${postId}`, {
+                method: 'DELETE',
+                headers: {
+                    "auth-token": token,
+                    'Content-Type': 'application/json',
+                }
+            });
+            const result = await response.json();
+            console.log(result);
+            if (!result.success) {
+                throw new Error('Request failed');
+            }
+            console.log(result);
+            dispatch(setTasks({ posts: result }));
+
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    };
+
+    const getMyPosts = async () => {
+        try {
+            const postResponse = await fetch(`${config.apiUrl}/task`, {
+                method: "GET",
+                headers: { 'auth-token': token }
+            });
+            const data = await postResponse.json()
+            dispatch(setTasks({ tasks: data.response }))
+
+        } catch (err) {
+            console.log("get feedPosts error", err)
+        }
+    }
+
+
+
+    const handleEditIconClick = async () => {
+        setIsEditMode(true);
+    }
+
+    const handleSaveIconClick = async () => {
+        try {
+            setIsEditMode(false);
+            setChecked(!isChecked)
+
+            const response = await fetch(`${config.apiUrl}/task/editTask/${postId}`, {
+                method: 'PATCH',
+                headers: {
+                    "auth-token": token,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ title: heading, description: descreption })
+            });
+            console.log(response);
+            if (!response.ok) {
+                throw new Error('Request failed');
+            }
+
+            const editedPost = await response.json();
+            console.log(editedPost);
+            dispatch(setTask({ task: editedPost }));
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    };
+
+    useEffect(() => { getMyPosts() }, [deleteClicked])
+    return (
+        <div style={completed ? taskcheckedItemStyles : taskItemStyles}>
+            <FormControlLabel
+                control={<Checkbox />}
+                checked={completed}
+                onChange={checkStatus}
+                label={completed ? "Completed" : 'Mark as complete'}
+                style={{ marginBottom: '5px' }}
+            />
+
+            <div style={iconsContainerStyles}>
+                {isEditMode ? (
+                    <CheckIcon style={{ marginRight: '10px' }} onClick={handleSaveIconClick} />
+                ) : (
+                    <EditIcon style={{ marginRight: '10px' }} onClick={handleEditIconClick} />
+                )}
+                <DeleteIcon onClick={handleDelete} />
+            </div>
+
+
+            {isEditMode ? (
+                <input
+                    type="text"
+                    value={heading}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            ) : (
+                <Typography variant="h6" style={titleStyles}>
+                    {title}
+                </Typography>
+            )}
+
+            {isEditMode ? (
+                <textarea
+                    value={descreption}
+                    onChange={(e) => setContent(e.target.value)}
+                    rows={4} // Adjust the number of rows as needed
+                    cols={5}
+                />
+            ) : (
+                <Typography style={{ fontStyle: 'italic' }}>{content}</Typography>
+            )}
+        </div>
+    );
+}
+
+export default TaskItem;
